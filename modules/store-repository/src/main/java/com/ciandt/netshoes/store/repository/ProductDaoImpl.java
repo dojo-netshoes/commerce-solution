ef77b80b8e12b163f5508bc525bfb0148a7ebb8d
Erick Rocha <erickpr@ciandt.com>
2016-03-08 16:54:27 -0300

    Signed-off-by: Erick Rocha <erickpr@ciandt.com>

---
+++ modules/store-repository/src/main/java/com/ciandt/netshoes/store/repository/ProductDaoImpl.java
@@ -0,0 +1,13 @@
+package com.ciandt.netshoes.store.repository;
+
+import com.ciandt.netshoes.store.dao.ProductDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by erickpr on 08/03/16.
+ */
+@Repository
+public class ProductDaoImpl implements ProductDao {
+
+
+}
